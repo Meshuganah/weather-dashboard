aed75b2d70ec58781ed1fe6f6aadc1b7aa98470d
Josh Klein <kleinjjosh74@gmail.com>
2022-03-26 15:04:22 -0400

    added information from API call to current day weather

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,7 @@
 var cityLat;
 var cityLon;
+var currentDayWeather = $(".current-day-weather");
+var searchedCity;
 
 //Function to get the latitude and longitude of a city, which will be passed to another API call later
 var getCityLocation = function(city) {
@@ -10,6 +12,8 @@ var getCityLocation = function(city) {
             response.json().then(function(data) {
                 cityLat = data[0].lat;
                 cityLon = data[0].lon;
+                searchedCity = "";
+                searchedCity = city;
                 getCityWeather(cityLat, cityLon);
             });
         } else {
@@ -20,12 +24,39 @@ var getCityLocation = function(city) {
 
 //Function that uses the lat/lon of searched city to find weater
 var getCityWeather = function(lat, lon) {
-    var apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&appid=e8cd9e5914172346c0997d3f67062c7d`;
+    var apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&units=imperial&appid=e8cd9e5914172346c0997d3f67062c7d`;
 
     fetch(apiUrl).then(function(response) {
         if(response.ok) {
             response.json().then(function(data) {
                 console.log(data);
+
+                //Creates a header for the searched city and the date of the current weater
+                var cityName = document.createElement("h4");
+                cityName.textContent = `${searchedCity}: ${moment().format("MM")}/${moment().format("DD")}/${moment().format("YYYY")}`;
+
+                //Creates a span for the current temperature
+                var cityTemp = document.createElement("span");
+                cityTemp.textContent = `Temp: ${data.current.temp} °F`;
+
+                //Creates a span for the current wind force
+                var cityWind = document.createElement("span");
+                cityWind.textContent = `Wind: ${data.current.wind_speed} MPH`;
+
+                //Creates a span for the current humidity
+                var cityHumid = document.createElement("span");
+                cityHumid.textContent = `Humidity: ${data.current.humidity}%`;
+
+                //Creates a span for the current UV Index
+                var cityUv = document.createElement("span");
+                cityUv.textContent = `UV Index: ${data.current.uvi}`;
+
+                //Appends all of the data gathered into the current weather container
+                currentDayWeather.append(cityName);
+                currentDayWeather.append(cityTemp);
+                currentDayWeather.append(cityWind);
+                currentDayWeather.append(cityHumid);
+                currentDayWeather.append(cityUv);
             });
         } else {
             alert("Error: City not found");
